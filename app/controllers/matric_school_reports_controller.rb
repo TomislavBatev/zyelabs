575f2c4391cb858d6540b7dfa76e6170846485b6
TomislavBatev <toma@tariffic.com>
2018-03-28 12:16:20 +0200

    add graphs

--- app/controllers/matric_school_reports_controller.rb
+++ app/controllers/matric_school_reports_controller.rb
@@ -1,13 +1,31 @@
 class MatricSchoolReportsController < ApplicationController
-  http_basic_authenticate_with name: "you_shall_not", password: "pass"
+  http_basic_authenticate_with name: "you_shall_not", password: "pass", except: :index
+
 
   def index
-    matric_school_reports = MatricSchoolReport.all
-    render json: matric_school_reports
+    @matric_school_reports = MatricSchoolReport.all
+
+
+    @total_schools = @matric_school_reports.count
+    @yearly_summaries = @matric_school_reports.group_by{ |r| r.year }.map do |year, results|
+      { year: year,
+        wrote_total: results.map(&:wrote).sum,
+        pass_total: results.map(&:passed).sum,
+        pass_average: (results.map(&:passed).sum.to_f * 100/results.map(&:wrote).sum.to_f).to_i
+      }
+    end
+
+    @histoplot_2014 = @matric_school_reports.where(year: 2016).where("wrote > 0").group_by{ |r| (r.passed.to_f * 100/r.wrote.to_f).to_i}.map{ |percentage, results| [percentage, results.count]}.sort
+    @histoplot_2015 = @matric_school_reports.where(year: 2015).where("wrote > 0").group_by{ |r| (r.passed.to_f * 100/r.wrote.to_f).to_i}.map{ |percentage, results| [percentage, results.count]}.sort
+    @histoplot_2016 = @matric_school_reports.where(year: 2014).where("wrote > 0").group_by{ |r| (r.passed.to_f * 100/r.wrote.to_f).to_i}.map{ |percentage, results| [percentage, results.count]}.sort
+
+    respond_to do |format|
+      format.html
+      format.json { @matric_school_reports }
+    end
   end
 
   def show
-    binding.pry
     matric_school_reports = MatricSchoolReport.find(matric_school_report_id)
     render json: matric_school_reports
   end
